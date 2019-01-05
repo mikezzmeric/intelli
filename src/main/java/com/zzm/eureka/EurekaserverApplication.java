6cad4ed7c24405e3d4f17fefd55466b3582e8e7f
mikezzmeric <396688706@qq.com>
2019-01-05 17:04:34 +0800

    提交着玩儿

--- src/main/java/com/zzm/eureka/EurekaserverApplication.java
+++ src/main/java/com/zzm/eureka/EurekaserverApplication.java
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication 
 {
 	public static void main(String[] args) {
-	    System.out.println("haha");
+	    System.out.println("haha1");
         SpringApplication.run(EurekaserverApplication.class, args);
     }
 }
